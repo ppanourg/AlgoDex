cc122c0f5448e5388613f482de8b5ef856dc6ec8
justin h <justin h>
2017-03-16 20:46:50 -0700

    big commit

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ def main():
     os.system("cp *.html ./public")
     #os.system("cp *.jpg ./public")
     #os.system("cp *.png ./public")
-    os.system("cp *.css ./public")
+    os.system("cp ./style/*.css ./public")
     os.system("cp -rf ./images ./public")
     os.system("cp *.js ./public");
     os.system("firebase deploy")
@@ -15,7 +15,7 @@ def main():
     os.system("copy *.html public")
     #os.system("copy *.jpg public")
     #os.system("copy *.png public")
-    os.system("copy *.css public")
+    os.system("copy style public\style")
     os.system("copy images public\images")
     os.system("copy *.js public");
     os.system("firebase deploy")
